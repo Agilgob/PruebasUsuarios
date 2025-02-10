577250ceb315613f5ece009bacd2cd90cf7a35cc
Manuel Valdez <manuel@agilgob.com>
2025-02-10 16:21:31 -0600

    Pruebas de parametros en el contenedor y construccion de la imagen con el hash

---
+++ Docker/Cypress/build_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Obtener el hash del último commit (primeros 15 caracteres)
+HASH=$(git rev-parse HEAD | cut -c1-15)
+IMAGE_NAME="cypress-agilgob:$HASH"
+
+# Verificar si existe una imagen con el mismo hash
+EXISTING_IMAGE=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep "cypress-agilgob:" | awk -F: '{print $2}')
+
+if [[ -z "$EXISTING_IMAGE" ]]; then
+    echo "No hay una imagen previa. Construyendo $IMAGE_NAME ..."
+    docker build -t $IMAGE_NAME -f Dockerfile .
+else
+    if [[ "$EXISTING_IMAGE" == "$HASH" ]]; then
+        echo "La imagen actual ya está actualizada con el hash $HASH. No es necesario construir."
+    else
+        echo "Eliminando imagen previa: cypress-agilgob:$EXISTING_IMAGE"
+        docker rmi "cypress-agilgob:$EXISTING_IMAGE"
+        echo "Construyendo nueva imagen: $IMAGE_NAME"
+        docker build -t $IMAGE_NAME -f Dockerfile .
+    fi
+fi
