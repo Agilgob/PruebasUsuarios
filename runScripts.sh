fe97a1e11af96b6c46f37678f9fe9da76c8b8b2e
ManuelValdez <manuel@agilgob.com>
2025-04-08 09:56:07 -0600

    Se agrega Penal a script shell

--- runScripts.sh
+++ runScripts.sh
@@ -6,6 +6,13 @@ run_test() {
   npx cypress run --spec "$spec" --env jsonFile=true --quiet
 }
 
+echo 'Eliminando archivos de salida...'
+rm -rf tmp
+
+echo "▶️ Ejecutando pruebas de Playwright en segundo plano"
+cantInputados=10 cantVictimas=10 ciudadano=ciudadanoJoseC2 npx playwright test playwright/tests/Penal.spec.js & 
+
+
 for test in \
     cypress/e2e/Exploratorios/001_DatosDemandadoSeOcultan.cy.js \
     cypress/e2e/Exploratorios/002_loginCredencialesIncorrectas.cy.js \
@@ -16,13 +23,13 @@ for test in \
     npx cypress run --spec $test --env funcionario=secretarioAcuerdos01,tramite=civiles_familiares_mercantiles_abogado_demandado
 done
 
-
+l
 if [ $? -eq 0 ]; then
 
     run_test cypress/e2e/Funcionario/001_IniciarCancelaTramite.cy.js 
     run_test cypress/e2e/Funcionario/002_EliminaTramitesIniciados.cy.js 
-    run_test cypress/e2e/Funcionario/003_IniciaTramite.cy.js -
-    run_test cypress/e2e/Funcionario/007_ActionButtonsEnabled.cy.js -
+    run_test cypress/e2e/Funcionario/003_IniciaTramite.cy.js
+    run_test cypress/e2e/Funcionario/007_ActionButtonsEnabled.cy.js
     run_test cypress/e2e/Funcionario/008_AcuerdosNotificacionesSentenciasOficios.cy.js 
 
     # los acuerdos requieren de promociones ingresadas
@@ -64,24 +71,24 @@ if [ $? -eq 0 ]; then
     run_test cypress/e2e/Funcionario/026_PromocionesPendientes.cy.js 
 
     # los acuerdos requieren de promociones ingresadas
-    run_test --spec cypress/e2e/Funcionario/009_AgregarDocumentoPromocion.cy.js 
+    run_test cypress/e2e/Funcionario/009_AgregarDocumentoPromocion.cy.js 
     if [ $? -eq 0 ]; then
-        run_test--spec cypress/e2e/Funcionario/027_AgregarAcuerdo-PublicarBoletin.cy.js 
+        run_test cypress/e2e/Funcionario/027_AgregarAcuerdo-PublicarBoletin.cy.js 
     else
         echo "Hubo un error en la carga de la promocion en la prueba 009 de funcionario"
     fi
 
-    run_test --spec cypress/e2e/Funcionario/028_Reportes.cy.js 
-    run_test --spec cypress/e2e/Funcionario/029_PanelFimasPendientes.cy.js 
-    run_test --spec cypress/e2e/Funcionario/014_TurnarExpediente-Interno.cy.js 
+    run_test cypress/e2e/Funcionario/028_Reportes.cy.js 
+    run_test cypress/e2e/Funcionario/029_PanelFimasPendientes.cy.js 
+    run_test cypress/e2e/Funcionario/014_TurnarExpediente-Interno.cy.js 
 
     if [ $? -eq 0 ]; then
-        run_test --spec cypress/e2e/Funcionario/501_RecibirExpedienteTurnado.cy.js 
-        run_test --spec cypress/e2e/Funcionario/007_ActionButtonsEnabled.cy.js 
-        run_test --spec cypress/e2e/Funcionario/008_AcuerdosNotificacionesSentenciasOficios.cy.js 
-        run_test --spec cypress/e2e/Funcionario/009_AgregarDocumentoPromocion.cy.js 
-        run_test --spec cypress/e2e/Funcionario/010_HabilitaVistaDocumentosACiudadano.cy.js 
-        run_test --spec cypress/e2e/Funcionario/023_TurnarExpediente-Externo.cy.js 
+        run_test cypress/e2e/Funcionario/501_RecibirExpedienteTurnado.cy.js 
+        run_test cypress/e2e/Funcionario/007_ActionButtonsEnabled.cy.js 
+        run_test cypress/e2e/Funcionario/008_AcuerdosNotificacionesSentenciasOficios.cy.js 
+        run_test cypress/e2e/Funcionario/009_AgregarDocumentoPromocion.cy.js 
+        run_test cypress/e2e/Funcionario/010_HabilitaVistaDocumentosACiudadano.cy.js 
+        run_test cypress/e2e/Funcionario/023_TurnarExpediente-Externo.cy.js 
     else
         echo "Hubo un error en el turnado interno del expediente en la prueba 014 de funcionario"
     fi
