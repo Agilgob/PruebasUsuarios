2ab433cac8a2db31aed4dc5e86606a4279b009c5
ManuelValdez <manuel@agilgob.com>
2025-05-06 13:10:13 -0600

    Cambios necesarios para las variables de entorno

--- cypress.config.js
+++ cypress.config.js
@@ -1,32 +1,17 @@
-const { defineConfig } = require("cypress");
-const dotenv = require("dotenv");
-const fs = require("fs");
-
+const { defineConfig } = require('cypress');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const { parseFunctionary, parseCitizen, parseEnvironment } = require('./cypress/utilities/config.js');
 
 const envFile = `.env.${process.env.ENV || 'sandbox'}`;
 
 if (fs.existsSync(envFile)) {
-    dotenv.config({ path: envFile });
+  dotenv.config({ path: envFile });
 } else {
-    throw new Error(`Archivo de entorno ${envFile} no encontrado`);
+  throw new Error(`Archivo de entorno ${envFile} no encontrado`);
 }
-
-const FUNCTIONARY_KEY = process.env.FUNCTIONARY || 'SECRETARIO_ACUERDOS_01';
-const functionaryRaw = process.env[FUNCTIONARY_KEY];
-
-const CITIZEN_KEY = process.env.CITIZEN || "CIUDADANO_MANUEL";
-const citizenRaw = process.env[CITIZEN_KEY];
-
-const environmentRaw = process.env.ENVIRONMENT;
-
-if (!functionaryRaw || !citizenRaw || !environmentRaw) {
-    const error = `functionaryRaw: ${functionaryRaw}, citizenRaw: ${citizenRaw}, environmentRaw: ${environmentRaw}`;
-  throw new Error(error);
-}
-
 
 module.exports = defineConfig({
-
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     reportDir: 'tmp',
@@ -34,13 +19,13 @@ module.exports = defineConfig({
     json: true,
     embeddedScreenshots: true,
     ignoreVideos: false,
-    screenshotsFolder: 'tmp/screenshots',  
+    screenshotsFolder: 'tmp/screenshots',
     videosFolder: 'tmp/videos',
     saveAllAttempts: true,
     overwrite: false,
     autoCleanReports: false,
-    reportFilename : "[status]-[name]",
-    debug: true    
+    reportFilename: '[status]-[name]',
+    debug: true,
   },
 
   e2e: {
@@ -48,21 +33,20 @@ module.exports = defineConfig({
       require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
-  
+
   defaultCommandTimeout: 10000,
   video: true,
   videoCompression: 32,
   videosFolder: 'tmp/videos',
-  screenshotsFolder: "tmp/screenshots",
+  screenshotsFolder: 'tmp/screenshots',
   downloadsFolder: 'tmp/downloads',
   trashAssetsBeforeRuns: false,
   redirectionLimit: 100,
   
-  env : {
-    funcionario: JSON.parse(functionaryRaw),
-    ciudadano: JSON.parse(citizenRaw),
-    environment: JSON.parse(environmentRaw),
-    tramite: "civiles_familiares_mercantiles_abogado_demandado"
-  }
-
+  env: {
+    funcionario: parseFunctionary(process.env.FUNCTIONARY || 'FUNC_LABORAL_ACUERDOS_02'),
+    ciudadano: parseCitizen(process.env.CITIZEN || 'CIUDADANO_MANUEL'),
+    environment: parseEnvironment(),
+    tramite: 'civiles_familiares_mercantiles_abogado_demandado',
+  },
 });
--- cypress/e2e/Ciudadano-Funcionario/ManejaValidaPermisosExpediente.cy.js
+++ cypress/e2e/Ciudadano-Funcionario/ManejaValidaPermisosExpediente.cy.js
@@ -28,10 +28,11 @@ describe('Documentos del expediente', () => {
     let expedientNumber = Cypress.env('expedientNumber');
     let functionaryExpedientUrl; 
 
-    describe('Validar permisos de los documentos desde el funcionario', () => {
+    describe('01 FUNCIONARIO - Validar permisos de los documentos desde el funcionario', () => {
 
-        let permisosInicial = []; // Se carga en la primer prueba antes de cualquier cambio
-        let permisosFinal = []; // Se carga en la prueba de verificacion del historial
+
+        let permissionsFinal = []; // Se carga en la prueba de verificacion del historial
+        let documentsFinal = [];
 
         beforeEach(() => {
             cy.session('funcionario', () => {
@@ -40,7 +41,7 @@ describe('Documentos del expediente', () => {
             cy.visit(environment.funcionarioURL);
         })
 
-        it('FUNCIONARIO - Valida que los permisos se muestran correctamente en la tabla y el modal del documento', () => {
+        it('Valida que los permisos se muestran correctamente en la tabla y el modal del documento', () => {
 
             // functionarySession();
             cy.hamburguer().click();
@@ -71,7 +72,7 @@ describe('Documentos del expediente', () => {
 
                 fetchDocumentPermissions(documents, Object.keys(documents), 0, {}, headers).then((_permissions) => {
                     permissions = _permissions;
-                    permisosInicial = {...permissions};
+                    // permisosInicial = {...permissions};
                     cy.writeFile('tmp/Permissions.json', permissions, { log: false });
                     functionaryValidatePermissions(documents, permissions, 0, ciudadano.email);
                 })
@@ -82,72 +83,73 @@ describe('Documentos del expediente', () => {
 
         });
 
-        it('FUNCIONARIO - Habilita todos los permisos del documento', () => {
+        it('Habilita todos los permisos del documento', () => {
             cy.visit(functionaryExpedientUrl);
 
-            cy.intercept('POST', `**/api/v1/docuemnts_expedient/document_expedient_users_permissions/*`)
-            .as('postPermisosExpediente');
+            const validate = (interception) => {
+                expect(interception.response.statusCode).to.eq(200); 
+                expect(interception.request.body).to.have.property('users');
+                expect(interception.request.body.users).to.have.length.greaterThan(0);
+                expect(interception.request.body.users[0]).to.have.property('isPermissionEnabled');
+                expect(interception.request.body.users[0].isPermissionEnabled).to.be.true;
+            }
+            modifyAllDocumentPermissions(':checked', validate).then((permisosOtorgados) => {
+                cy.writeFile('tmp/PermisosOtorgados.json', permisosOtorgados, { log: false });
+            })
+            
+        })
 
+        it('Deshabilita todos los permisos del documento', () => {
+            cy.visit(functionaryExpedientUrl);
 
-            cy.get('.document-expedient-table.procedures-table-container').as('tablaDocumentos');
-            cy.get('@tablaDocumentos').find('tbody tr td.fixed-cell').as('tdControlDocumentos');
-            cy.get('@tdControlDocumentos').each((element) => {
-                cy.wrap(element).find('[title="Gestionar permisos"]').should('have.descendants', 'i').click()
-                cy.get('.list-group.list-group-flush').as('tablaPermisos');
-                cy.get('@tablaPermisos').should('be.visible');
-                cy.get('@tablaPermisos').find('.custom-control.custom-switch > input[type="checkbox"]')
-                    .then((checkbox) => {
-                        if (checkbox.is(':checked')) {
-                            cy.log('El permiso ya está habilitado para ' + element.text());
-                            cy.contains('button', 'Cerrar').click()
-                            cy.contains('button', 'Aceptar').click()
+            const validate = (interception) => {
+                expect(interception.response.statusCode).to.eq(200); 
+                expect(interception.request.body).to.have.property('users');
+                expect(interception.request.body.users).to.have.length.greaterThan(0);
+                expect(interception.request.body.users[0]).to.have.property('isPermissionEnabled');
+                expect(interception.request.body.users[0].isPermissionEnabled).to.be.false;
+            }
 
-                        } else {
-                            cy.get('@tablaPermisos').find('.custom-control.custom-switch').click();
-                            cy.contains('button', 'Guardar').click()
-                            cy.contains('button', 'Aceptar').click()
-                            cy.wait('@postPermisosExpediente').then((interception) => {
-                                //  Verificar que la petición se hizo correctamente
-                                expect(interception.response.statusCode).to.eq(200); 
-
-                                //  Revisar el request enviado
-                                // cy.log(`Méthod: ${interception.request.method}`);
-                                // cy.log(`URL: ${interception.request.url}`);
-                                // cy.log(`Headers: ${JSON.stringify(interception.request.headers, null, 2)}`);
-                                // cy.log(`Body: ${JSON.stringify(interception.request.body, null, 2)}`);
-                                // TODO acumular en un array los permisos otorgados
-                                expect(interception.request.body).to.have.property('users');
-                                expect(interception.request.body.users).to.have.length.greaterThan(0);
-                                expect(interception.request.body.users[0]).to.have.property('isPermissionEnabled');
-                                expect(interception.request.body.users[0].isPermissionEnabled).to.be.true;
-                            });
-                        }
-                    })            
+            modifyAllDocumentPermissions(':not(:checked)', validate).then((permisosDenegados) => {
+                cy.writeFile('tmp/PermisosDenegados.json', permisosDenegados, { log: false });
             })
         })
 
-        it('FUNCIONARIO - Deshabilita todos los permisos del documento', () => {
-            // TODO Captura en un array los permisos retirados
-        })
+        it('Verifica que el historial de cambios se registra y muesrtra correctamente', () => {
 
-        it('FUNCIONARIO - Verifica que el historial de cambios se muestra correctamente', () => {
-            // Captura el historial en la primer prueba 
-            // Cambia el estado del documento 
-            // Captura el historial en esta prueba 
-            // Refresca pantalla, logout, login luego captura el historial
-            // Compara los cambios
             cy.intercept('GET', '**/api/v1/document_expedients/documents/*/10?page=1').as('getDocuments');
             cy.visit(functionaryExpedientUrl);
             cy.wait('@getDocuments').then((interception) => {
-                documents = interception.response.body.data.documents.reduce((acc, item) => {
+
+                const data = interception.response.body.data;
+
+                documentsFinal = data.documents.reduce((acc, item) => {
                     acc[item.id] = item;
                     return acc;
                     }, {});
+                
+                cy.writeFile('tmp/DocumentsFinal.json', documentsFinal , { log: false });
 
-                fetchDocumentPermissions(documents, Object.keys(documents), 0, {}, headers).then((_permissions) => {
-                    permisosFinal = _permissions;
+                const headers = interception.request.headers; 
+                fetchDocumentPermissions(documentsFinal, Object.keys(documentsFinal), 0, {}, headers).then((permissionsFinal) => {
+                    cy.writeFile('tmp/PermisosFinal.json', permissionsFinal, { log: false });
+                    functionaryValidatePermissions(documents, permissionsFinal, 0, ciudadano.email);
+                    functionaryValidateHistory(documentsFinal, permissions, permissionsFinal, ciudadano.email);
                 })
+
+            });
+        }) 
+
+    })
+
+    describe('02 Ciudadano - No debe ver los documentos del expediente', () => {
+        // Estas pruebas estan pensadas para que el ciudadano no pueda ver los documentos del expediente
+        // aunque no se generara un falso positivo si a caso se cuenta con un acuerdo en la promocion 
+        beforeEach(() => {
+            cy.session('ciudadano', () => {
+                citizenSession();
             })
+            cy.visit(environment.ciudadanoURL);
         })
 
     })
@@ -202,6 +204,42 @@ function fetchDocumentPermissions(documentsDict = {}, keys = Object.keys(documen
 }
 
 
+function functionaryValidateHistory(documents = {}, permissionsThen = {}, permissionsNow = {}, citizenEmail = ''){
+
+
+    function validateHistoryStructure(historyObject = Object) {
+
+        expect(historyObject).to.have.all.keys('id', 'email', 'username', 'fullname', 'isPermissionEnabled', 'visibility_date', 'history');
+        historyObject.history.forEach((entry) => {
+            expect(entry).to.have.all.keys('active', 'updated_at');
+            expect(entry.active).to.be.a('boolean');
+            expect(entry.updated_at).to.be.a('string');
+          });
+    }
+
+    function iterate(index = 0, documents = Object, permissionsThen = Object, permissionsNow = Object, citizenEmail = String) {
+        if (index >= Object.keys(documents).length) {
+            return;
+        }
+
+        const document = documents[Object.keys(documents)[index]];
+        const documentPermissionsThen = filterPermission(document, permissionsThen, citizenEmail);
+        const documentPermissionsNow = filterPermission(document, permissionsNow, citizenEmail);
+
+        validateHistoryStructure(documentPermissionsThen);
+        validateHistoryStructure(documentPermissionsNow);
+        expect(documentPermissionsThen.history.length).to.be.greaterThan(0); // History cant be empty at the beginning
+        expect(documentPermissionsNow.history.length).to.be.greaterThan(0); // History cant be empty at the end
+        expect(documentPermissionsThen.history[0].active).to.eq(documentPermissionsNow.isPermissionEnabled); // Last history must be the same as the current status
+        expect(documentPermissionsThen.history.length).to.lessThan(documentPermissionsNow.history.length); // History count at the beginning must be less than the history count at the end
+
+        iterate(index + 1, documents, permissionsThen, permissionsNow, citizenEmail); // Recursive call to check the next document
+    }
+
+    iterate(0, documents, permissionsThen, permissionsNow, citizenEmail); 
+}
+
+
 
 function functionaryValidatePermissions(documents = Object, permissions = Object, index = 0, citizenEmail = String) {
     
@@ -227,7 +265,6 @@ function functionaryValidatePermissions(documents = Object, permissions = Object
         .should('be.visible')
         .as('documentRow');
 
-    
     // Check if the document has the button to download/view
     cy.get('@documentRow').find('[title="Descargar / Visualizar"]').should('exist').should('be.visible')
     
@@ -249,6 +286,14 @@ function functionaryValidatePermissions(documents = Object, permissions = Object
 }
 
 
+/**
+ * Filters and retrieves a specific user's permission for a given document.
+ *
+ * @param {Object} document - The document object containing an `id` property.
+ * @param {Object} permissions - An object where keys are document IDs and values are permission objects.
+ * @param {string} citizenEmail - The email of the citizen whose permission is being searched for.
+ * @returns {Object|undefined} - The permission object for the specified user, or `undefined` if not found.
+ */
 function filterPermission(document = Object, permissions = Object, citizenEmail = String) {
     const permissionObj = permissions[document.id];
     return permissionObj.users.find(
@@ -256,56 +301,50 @@ function filterPermission(document = Object, permissions = Object, citizenEmail 
     );
 }
 
-function expedientHaveAgreements(expedient = Object) {
-    
-}
 
-
-
-
-const modifyAllDocumentPermissions = (desiredStatus = ':checked', validate = (interception) => {}) => {
-    cy.intercept('POST', `**/api/v1/docuemnts_expedient/document_expedient_users_permissions/*`)
-    .as('postPermisosExpediente');
-
-
+/**
+ * Modifies the permissions of all documents in an expedient based on the desired status.
+ * 
+ * @param {string} [desiredStatus=':checked'] - The expected status of the checkboxes. 
+ *                                              It can be `:checked` or `:not(:checked)`.
+ * @param {function} [validate=() => {}] - A callback function to validate the `interception` 
+ *                                         of the POST request that modifies the document permissions.
+ *                                         The `interception` object contains details about the request and response.
+ * @returns {Cypress.Chainable<Array>} - A Cypress chainable that resolves to an array of request bodies 
+ *                                       sent during the POST requests for modifying permissions.
+ */
+const modifyAllDocumentPermissions = (desiredStatus = ':checked', validate = () => {}) => {
+    const results = [];
+  
+    cy.intercept('POST', '**/api/v1/docuemnts_expedient/document_expedient_users_permissions/*')
+      .as('postPermisosExpediente');
+  
     cy.get('.document-expedient-table.procedures-table-container').as('tablaDocumentos');
     cy.get('@tablaDocumentos').find('tbody tr td.fixed-cell').as('tdControlDocumentos');
-    let results = [];
+  
     cy.get('@tdControlDocumentos').each((element) => {
-        cy.wrap(element).find('[title="Gestionar permisos"]').should('have.descendants', 'i').click()
-        cy.get('.list-group.list-group-flush').as('tablaPermisos');
-        cy.get('@tablaPermisos').should('be.visible');
-        cy.get('@tablaPermisos').find('.custom-control.custom-switch > input[type="checkbox"]')
-            .then((checkbox) => {
-                if (checkbox.is(desiredStatus)) {
-                    cy.log('El permiso ya está habilitado para ' + element.text());
-                    cy.contains('button', 'Cerrar').click()
-                    cy.contains('button', 'Aceptar').click()
-
-                } else {
-                    cy.get('@tablaPermisos').find('.custom-control.custom-switch').click();
-                    cy.contains('button', 'Guardar').click()
-                    cy.contains('button', 'Aceptar').click()
-                    cy.wait('@postPermisosExpediente').then((interception) => {
-
-                        validate(interception);
-                        results.concat(interception.request.body);
-                        //  Verificar que la petición se hizo correctamente
-                        // expect(interception.response.statusCode).to.eq(200); 
-
-                        // //  Revisar el request enviado
-                        // // cy.log(`Méthod: ${interception.request.method}`);
-                        // // cy.log(`URL: ${interception.request.url}`);
-                        // // cy.log(`Headers: ${JSON.stringify(interception.request.headers, null, 2)}`);
-                        // // cy.log(`Body: ${JSON.stringify(interception.request.body, null, 2)}`);
-                        // // TODO acumular en un array los permisos otorgados
-                        // expect(interception.request.body).to.have.property('users');
-                        // expect(interception.request.body.users).to.have.length.greaterThan(0);
-                        // expect(interception.request.body.users[0]).to.have.property('isPermissionEnabled');
-                        // expect(interception.request.body.users[0].isPermissionEnabled).to.be.true;
-                    });
-                }
-            })            
-    })
-    return cy.wrap(results);
-}
+      cy.wrap(element).find('[title="Gestionar permisos"]').should('have.descendants', 'i').click();
+      cy.get('.list-group.list-group-flush').as('tablaPermisos');
+      cy.get('@tablaPermisos').should('be.visible');
+      cy.get('@tablaPermisos')
+        .find('.custom-control.custom-switch > input[type="checkbox"]')
+        .then((checkbox) => {
+          if (checkbox.is(desiredStatus)) {
+            cy.log('El permiso ya está habilitado para ' + element.text());
+            cy.contains('button', 'Cerrar').click();
+            cy.contains('button', 'Aceptar').click();
+          } else {
+            cy.get('@tablaPermisos').find('.custom-control.custom-switch').click();
+            cy.contains('button', 'Guardar').click();
+            cy.contains('button', 'Aceptar').click();
+            cy.wait('@postPermisosExpediente').then((interception) => {
+              validate(interception);
+              results.push(interception.request.body);
+            });
+          }
+        });
+    });
+  
+    return cy.then(() => results); 
+  };
+  
