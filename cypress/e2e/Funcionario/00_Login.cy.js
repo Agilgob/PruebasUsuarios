6c072502a2db4c78d9c6db240625390e1a97f4fa
Manuel valdez <manuel@agilgob.com>
2025-06-20 10:47:25 -0600

    Correccion menor en la prueba de login funcionario

--- cypress/e2e/Funcionario/00_Login.cy.js
+++ cypress/e2e/Funcionario/00_Login.cy.js
@@ -1,11 +1,10 @@
 import { loadTestData } from "../../support/loadTestData";
 
 
-describe('Inicia el tramite luego lo cancela', () => {
+describe('Prueba basica de login', () => {
     
-    before(() => { 
-        loadTestData();
-    })
+    const funcionario = Cypress.env('funcionario');
+    const environment = Cypress.env('environment');
 
     beforeEach(() => {
         cy.session('sesionFuncionario', () => {
@@ -16,25 +15,13 @@ describe('Inicia el tramite luego lo cancela', () => {
             cy.loginFuncionario(funcionario.email, funcionario.password);
             cy.wait(2000);
             
-        }, {
-            validate() {
-                cy.getAllLocalStorage().then((storage) => {
-
-                    let expDate = storage['https://sandbox.funcionario.cjj.gob.mx']["tokenExpirationDateStored"]; 
-                    let parsedExpDate = Date.parse(expDate) - 60000; // se restan 30s del prompt y 30 para la prueba
-                    cy.log(parsedExpDate); 
-                    expect(parsedExpDate).to.be.greaterThan(Date.now());
-
-                })
-                cy.getCookie('authentication_token_03', { timeout: 5000 }).should('exist');
-            },
-            cacheAcrossSpecs: true,
         });
 
     })
 
     it("prueba para inicion de sesion" , () => {
-        cy.visit(environment.funcionarioURL);
+        cy.visit(environment.funcionarioURL); // TODO Agregar validaciones de los endpoints que se consultan al iniciar sesión
+        cy.getCookie('authentication_token_03', { timeout: 5000 }).should('exist');
     })
 
 
